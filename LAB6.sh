cc7b69570413c949bac33c393dd9252e9f4998ab
mformanek <mourrison@live.com>
2019-04-22 16:12:34 -0600

    pre final

--- LAB6.sh
+++ LAB6.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 # ------------------------------------------------------------------
-# [Author] Title
-#          Description
-#
-#          This script uses shFlags -- Advanced command-line flag
-#          library for Unix shell scripts.
-#          http://code.google.com/p/shflags/
-#
-# Dependency:
-#     http://shflags.googlecode.com/svn/trunk/source/1.0/src/shflags
+# By Milan Formanek 	LAB6 Deployment Script
 # ------------------------------------------------------------------
 VERSION=0.1.0
 SUBJECT=some-unique-id
-USAGE="Usage: command -hv args"
+USAGE="Run on the individual DM machines with the letter name of the machine as the parameter."
 
 if [ $# == 0 ] ; then
     echo $USAGE
     exit 1;
 fi
 
-if [ $1 == "OFF" ] ; then #ENABLE EVERITHING IN IPTABLES
+if [ $1 == "OFF" ] ; then #ENABLE EVERITHING IN IPTABLES for testing
     iptables -P INPUT ACCEPT 
     iptables -P FORWARD ACCEPT 
     iptables -P OUTPUT ACCEPT
@@ -30,7 +22,7 @@ fi
 
 iptables -F #reset IPTABLES
 
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT #Allow return packets for ESTABLISHED and RELATED packets
 
 iptables -P INPUT DROP # set default DROP policy 
 iptables -P OUTPUT ACCEPT
@@ -54,11 +46,11 @@ fi
 if [ $1 != "A" ] ; then
 	iptables -P FORWARD DROP #disble forwarding on non routers
 else #RULES FOR ROUTER/MACHINE A
-    iptables -P FORWARD ACCEPT #enable forwarding on routers
-    iptables -A FORWARD -s 157.240.28.35 -j DROP
-    iptables -A FORWARD -d 157.240.28.35 -j DROP #block FACEBOOK
-    iptables -A FORWARD -s 216.176.177.74 -j DROP
-    iptables -A FORWARD -d 216.176.177.74 -j DROP #block CHEESEBURGER.com
+    	iptables -P FORWARD ACCEPT #enable forwarding on routers
+    	iptables -A FORWARD -s 157.240.28.35 -j DROP
+    	iptables -A FORWARD -d 157.240.28.35 -j DROP #block FACEBOOK
+    	iptables -A FORWARD -s 216.176.177.74 -j DROP
+    	iptables -A FORWARD -d 216.176.177.74 -j DROP #block CHEESEBURGER.com
     
     	iptables -A FORWARD -p tcp -s  100.64.0.0/16 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 	iptables -A FORWARD -p tcp -s  10.21.32.0/24 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
@@ -114,6 +106,7 @@ if [ $1 == "D" ] ; then #RULES FOR MACHINE D - DNS SERVER
 	iptables -A OUTPUT -p udp -s $SERVER_IP --sport 53 -d 0/0 --dport 53 -m state --state ESTABLISHED -j ACCEPT 
 	#allow inbound DNS lookup on chase.
 fi
+
 if [ $1 == "E" ] ; then #RULES FOR MACHINE E - FILE SERVER
 	iptables -A INPUT -p tcp -s 10.21.32.0/24 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 	iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT #enable SSH connections only from 10.21.32.0/24 net
