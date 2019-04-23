073f5f431551c663465118b9b89576852bd54f84
mformanek <mourrison@live.com>
2019-04-22 22:12:05 -0600

    A10

--- LAB6.sh
+++ LAB6.sh
@@ -33,7 +33,7 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type time-exceeded -j ACCEPT
-iptables -A INPUT -p icmp --icmp-type destination-unreachable -j ACCEPT #ACCEPT ICMP packets
+iptables -A INPUT -p icmp --icmp-type destination-unreachable -j ACCEPT #ACCEPT ICMP packets.
 
 if [ $1 != "E" ] ; then
 	iptables -A INPUT -p tcp -s  100.64.0.0/16 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
